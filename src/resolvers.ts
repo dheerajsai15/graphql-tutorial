021e18dca200a1c9bbcd0e2939621bed4270aa3d
Dheeraj Sai <dheerajsaivnr@gmail.com>
2026-03-10 20:35:25 +0530

    Added new entry point to Query

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -4,6 +4,9 @@ export const resolvers: Resolvers = {
     Query: {
         featuredListings: (_, __, { dataSources }) => {
             return dataSources.listingAPI.getFeaturedListings()
+        },
+        listing: (_, { id }, { dataSources }) => {
+            return dataSources.listingAPI.getListing(id)
         }
     }
 }

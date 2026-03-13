d0cd0dd2f10432ffafd7c5cd4662105a28f4f90c
Dheeraj Sai <dheerajsaivnr@gmail.com>
2026-03-13 22:45:19 +0530

    Added resolver chaining

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,4 +1,5 @@
 import { Resolvers } from "./types"
+import { validateFullAmenities } from "./helpers";
 
 export const resolvers: Resolvers = {
     Query: {
@@ -7,6 +8,13 @@ export const resolvers: Resolvers = {
         },
         listing: (_, { id }, { dataSources }) => {
             return dataSources.listingAPI.getListing(id)
+        }
+    },
+    Listing: {
+        amenities: ({ id, amenities }, _, { dataSources }) => {
+            return validateFullAmenities(amenities)
+                ? amenities
+                : dataSources.listingAPI.getAmenities(id)
         }
     }
 }
